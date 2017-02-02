193d39738ab3b4d449613fb7e5a227f0320067fd
Cemalettin Koc <cemalettin.koc@gmail.com>
2017-02-03 01:48:16 +0200

    polished script

--- bin/build_docker_images_and_push.sh
+++ bin/build_docker_images_and_push.sh
@@ -3,36 +3,36 @@
 docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USER} -p ${DOCKER_PASS}
 
 last_commit_hash=$(git rev-parse --short HEAD 2> /dev/null )
-echo "**************************************************************************** "
-echo "************** Last commit =  ${last_commit_hash} ************************** "
-echo "**************************************************************************** "
+echo "****************************************************************************"
+echo "************** Last commit =  ${last_commit_hash} **************************"
+echo "****************************************************************************"
 cd ${APPS_FOLDER}
 
 [[ !  -z  ${SELECTED_APPS} ]] && apps="${SELECTED_APPS[@]}" || apps="*"
 
 for app in ${apps}; do
   if [ -d ${app} ]; then
-    echo "**************************************************************************** "
-    echo "************** Building ${app} with docker and shade profiles ************** "
-    echo "**************************************************************************** "
+    echo "****************************************************************************"
+    echo "************** Building ${app} with docker and shade profiles **************"
+    echo "****************************************************************************"
     docker_image_name=$DOCKER_USER/${app}
 
-    echo "***************************************************************************** "
-    echo "************** Building docker image with name = ${docker_image_name} ************** "
-    echo "***************************************************************************** "
+    echo "****************************************************************************"
+    echo "************** Building docker image with name = ${docker_image_name} ******"
+    echo "****************************************************************************"
     docker build -t ${docker_image_name}:${last_commit_hash} ${app}/target/docker
     docker tag ${docker_image_name}:${last_commit_hash} ${docker_image_name}:latest
 
-    echo "***********************************************************************"
-    echo "************** Pushing docker image ${docker_image_name} **************"
-    echo "***********************************************************************"
+    echo "****************************************************************************"
+    echo "************** Pushing docker image ${docker_image_name} *******************"
+    echo "****************************************************************************"
     docker push ${docker_image_name}
    
-    echo "***********************************************************************"
-    echo "************** Deploying docker image ${docker_image_name} **************"
-    echo "***********************************************************************"   
-    if [ -x "$(command -v kubectl)" ]; then
-      kubectl patch deployment ${app} -p '{"spec":{"template":{"spec":{"containers":[{"name":"backend-container","image":"'$docker_image_name':'$last_commit_hash'"}]}}}}' -n=test
+    if hash kubectl 2>/dev/null; then
+        echo "****************************************************************************"
+        echo "************** Deploying docker image ${docker_image_name} *****************"
+        echo "****************************************************************************"
+        kubectl patch deployment ${app} -p '{"spec":{"template":{"spec":{"containers":[{"name":"backend-container","image":"'$docker_image_name':'$last_commit_hash'"}]}}}}' -n=test
     fi
   fi
 done
