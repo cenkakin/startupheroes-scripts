744fcf13e3316ee3fd2b456e814efd9ac8b5ffe1
pasali <mhmtbsl@gmail.com>
2017-03-10 11:21:24 +0300

    changed code push env variable name

--- bin/react-app.sh
+++ bin/react-app.sh
@@ -3,7 +3,9 @@
 npm install
 npm install -g code-push-cli
 code-push login --accessKey ${CODE_PUSH_ACCESS_KEY}
-code-push release-react ${CLIENT_APP_NAME} android -d "Staging" --dev false
-./android/gradlew dependencies
-./android/gradlew assembleInternalRelease assembleProductionRelease crashlyticsUploadDistributionInternalRelease crashlyticsUploadDistributionProductionRelease
+code-push release-react ${CODE_PUSH_REPO_NAME} android -d "Staging" --dev false
+cd android
+./gradlew dependencies
+./gradlew assembleInternalRelease assembleProductionRelease crashlyticsUploadDistributionInternalRelease crashlyticsUploadDistributionProductionRelease
+cd ..
 
