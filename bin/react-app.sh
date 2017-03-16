9beafab5e3eeb47afe00aa13dac347b4ea1a2135
cenk akın <cenk.akin91@gmail.com>
2017-03-16 09:21:53 +0300

    Update react-app.sh

--- bin/react-app.sh
+++ bin/react-app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-npm install
+npm install --production
 npm install -g code-push-cli
 code-push login --accessKey ${CODE_PUSH_ACCESS_KEY}
 code-push release-react ${CODE_PUSH_REPO_NAME} android -d "Staging" --dev false
