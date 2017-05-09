ad6cbb7cd0c5f83f95c2587afbda96c36ef9f4f5
pasali <mhmtbsl@gmail.com>
2017-05-09 22:18:18 +0300

    polished sonarqube trigger

--- bin/trigger_sonarqube.sh
+++ bin/trigger_sonarqube.sh
@@ -2,7 +2,9 @@
 
 curl -X POST --header "Content-Type: application/json" -d '{
   "build_parameters": {
-      "text" : "'${CI_PULL_REQUEST##*/}' '${CIRCLE_PROJECT_USERNAME}'/'${CIRCLE_PROJECT_REPONAME}' '${CIRCLE_BRANCH}'"
+      "PR_NUMBER" : "'${CI_PULL_REQUEST##*/}'",
+      "BRANCH":"'${CIRCLE_BRANCH}'",
+      "REPOSITORY":"'${CIRCLE_PROJECT_USERNAME}'/'${CIRCLE_PROJECT_REPONAME}'"
   }
 }
 ' "https://circleci.com/api/v1.1/project/github/${SONAR_SCANNER_REPOSITORY}?circle-token=${CIRCLE_TOKEN}"
